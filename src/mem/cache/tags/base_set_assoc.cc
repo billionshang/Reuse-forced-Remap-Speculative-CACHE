1012af858cc1fd8298605e3e6d243e1e92473a81
shang xiaojing <38311611+billionshang@users.noreply.github.com>
2019-03-14 15:31:59 +0800

    remap_new

--- src/mem/cache/tags/base_set_assoc.cc
+++ src/mem/cache/tags/base_set_assoc.cc
@@ -137,24 +137,24 @@ BaseSetAssoc::BaseSetAssoc(const Params *p) //初始化过程所使用的函数
             blk->whenReady = 0;
             //sxj
             //for ssram
-            bool blkError = subblk[j][0] || subblk[j][1] || subblk[j][2] || subblk[j][3];
-            for(int iii = 0; iii < 4; iii++){
-                blk->weakMap[iii] = subblk[j][iii];//saving the weak map, only using for L1
-            }
-            if (blkError){
-                blk->isWeak = true;
-            }
+            // bool blkError = subblk[j][0] || subblk[j][1] || subblk[j][2] || subblk[j][3];
+            // for(int iii = 0; iii < 4; iii++){
+            //     blk->weakMap[iii] = subblk[j][iii];//saving the weak map, only using for L1
+            // }
+            // if (blkError){
+            //     blk->isWeak = true;
+            // }
 
             //for remap
-            // if (maxError){
-            //     blk->isWeak = true;
-            //     maxError--;
-            // }
-            // for(int iii = 0; iii < 4; iii++){
-            //     if (subblkErrorCnt[iii])
-            //         blk->weakMap[iii] = true;
-            //     subblkErrorCnt[iii]--;
-            // }
+            if (maxError){
+                blk->isWeak = true;
+                maxError--;
+            }
+            for(int iii = 0; iii < 4; iii++){
+                if (subblkErrorCnt[iii])
+                    blk->weakMap[iii] = true;
+                subblkErrorCnt[iii]--;
+            }
 
             //sxj end
             blk->isTouched = false;
